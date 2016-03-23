8de8b0291f561a20fd9f12f9a50e3b62969ab065
Brendan Koning <koningb@localhost.localdomain>
2016-03-22 22:01:04 -0400

    Minor cleanup

--- proj2.c
+++ proj2.c
@@ -20,7 +20,6 @@ void addToLine(int custCount, int idStart, int time, queue *q){
 		d.arriveTime = time;
 		enqueue(d, q);
 	}
-	//printf("%d customers arrived.\n", custCount);
 }
 
 
@@ -30,7 +29,7 @@ void simulation(int numOfTellers){
 
 	int tellers[numOfTellers];
 	int currentTime = 0;
-	int count = 0;
+	int currentId = 0;
 	queue line;
 	stats s;
 
@@ -43,8 +42,7 @@ void simulation(int numOfTellers){
 	srand((unsigned int)time(NULL));
 	
 	while(currentTime < MIN_PER_DAY){
-		//printf("\nStart minute %d\n", currentTime);
-
+		//Determines how many customers will arrive at this minute
 		int newCustRand = (rand() % 100) + 1;
 		int newCust = 0;
 		
@@ -57,18 +55,10 @@ void simulation(int numOfTellers){
 		else if(newCustRand < 70)
 			newCust = 3;
 		else
-			newCust = 4;	
-		
-		addToLine(newCust, count, currentTime, &line);
-		count = count + newCust;
-		//printf("%d Customers now in line.\n", line.cnt);
-		/*
-		if(!empty(&line)){
-			data temp = front(&line);
-
-			printf("Next in Line: ID: %d, Arrival %d\n", temp.id, temp.arriveTime); 		
-		}
-		*/
+			newCust = 4;
+	
+		addToLine(newCust, currentId, currentTime, &line);
+		currentId = currentId + newCust;
 		for(int i = 0; i < numOfTellers; i++){
 			if(tellers[i] != 0)
 				tellers[i] = tellers[i] - 1;
@@ -79,10 +69,6 @@ void simulation(int numOfTellers){
 					processPerson(&s, timeInLine);
 					int timeToServe = (int)ceil(expdist(AVG_SERVICE));
 					tellers[i] = timeToServe;
-					//printf("Teller %d is now helping customer %d. This will take %d minutes.\n", i, temp.id, timeToServe);
-				}
-				else{
-					//printf("Teller %d is open. No customers in line.\n", i);
 				}
 			}
 		}
